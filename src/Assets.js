a4e40809c4288c941803be93dda660daf9b683e6
izuru775 <izuru775@gmail.com>
2022-01-12 09:38:05 +1100

    Minor bug fix

--- src/Assets.js
+++ src/Assets.js
@@ -1,54 +1,60 @@
-import 'aframe';
-import React, {useState,useEffect} from 'react';
+import "aframe";
+import React, { useState, useEffect, Suspense } from "react";
+import { Scene } from "aframe-react";
+import ReactDOM from "react-dom";
 
 function MyComponent() {
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [items, setItems] = useState([]);
-  
-    // Note: the empty deps array [] means
-    // this useEffect will run once
-    // similar to componentDidMount()
-    useEffect(() => {
-      fetch("http://localhost:3001/api")
-        .then(res => res.json())
-        .then(
-          (result) => {
-            setIsLoaded(true);
-            setItems(result.data[0].vrObject[0]);
-          },
-          // Note: it's important to handle errors here
-          // instead of a catch() block so that we don't swallow
-          // exceptions from actual bugs in components.
-          (error) => {
-            setIsLoaded(true);
-            setError(error);
-          }
-        )
-    }, [])
-  
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {//one two
-        let modelUrl =
-      "https://foresthouse.s3.ap-southeast-2.amazonaws.com/forest_house/house.gltf";
-        console.log(items.url)
-      return (
-        <a-scene id="mainScene" background="color:black" environment="preset:forest">
-        <a-entity
-          scale={items.scale}
-          rotation={items.rotation}
-          position={items.position}
-          gltf-model="#foresthouse"
-        />
-        <a-assets id="assets">
-          <a-asset-item id="foresthouse" src={modelUrl} />
-        </a-assets>
-      </a-scene>
-      );
-    }
-  }
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
 
-  export default MyComponent;
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    fetch(
+      "http://ec2-3-26-220-121.ap-southeast-2.compute.amazonaws.com:3001/api"
+    )
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result.data[0].vrObject[0]);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    const url =
+      "https://foresthouse.s3.ap-southeast-2.amazonaws.com/forest_house/house.gltf";
+    return (
+      <Scene
+        id="mainScene"
+        background="color:black"
+        environment="preset:forest"
+      >
+        {/* <Entity text={{value: 'Hello, A-Frame React!', align: 'center',color:'black'}} position={{x: 0, y: 2, z: -1}}/> */}
+
+        <a-assets>
+          <a-asset-item  id="tree" src={url}></a-asset-item>
+        </a-assets>
+
+        <a-gltf-model position={items.position} scale={items.scale} rotation ={items.rotation} src="#tree"></a-gltf-model>
+      </Scene>
+      //this is sdsd
+    );
+  }
+}
+
+export default MyComponent;
